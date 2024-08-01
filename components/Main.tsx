b928922be020ef250bfa7428a269ca0e26298b65
ArchitGoyal2005 <architgoyal2005@gmail.com>
2024-08-01 19:42:03 +0530

    fixed basic styling issues

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import objectStyle from "@/public/objectStyle.json";
 import QuestionDisplay from "./QuestionDisplay";
@@ -6,70 +6,69 @@ import CSSEditor from "./CSSEditor";
 import PreviewBox from "./PreviewBox";
 
 interface StyleObject {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 interface Question {
-    id: number;
-    targetContainerHTML: string;
-    objectContainerHTML: string;
-    initialCSS: StyleObject;
-    instruction: string;
-    answer: string;
-    points: string;
-    difficulty: string;
+  id: number;
+  targetContainerHTML: string;
+  objectContainerHTML: string;
+  initialCSS: StyleObject;
+  instruction: string;
+  answer: string;
+  points: string;
+  difficulty: string;
 }
 
 const Main: React.FC = () => {
-    const questions: Question[] = objectStyle;
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [baseStyle, setBaseStyle] = useState<StyleObject>(questions[0].initialCSS);
-    const [userStyle, setUserStyle] = useState<StyleObject>({});
+  const questions: Question[] = objectStyle;
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [baseStyle, setBaseStyle] = useState<StyleObject>(
+    questions[0].initialCSS
+  );
+  const [userStyle, setUserStyle] = useState<StyleObject>({});
 
-    const handleStyleChange = (newStyle: StyleObject) => {
-        setUserStyle(newStyle);
-    }
+  const handleStyleChange = (newStyle: StyleObject) => {
+    setUserStyle(newStyle);
+  };
 
-    const handlePageChange = (index: number) => {
-        setCurrentQuestionIndex(index);
-        setBaseStyle(questions[index].initialCSS);
-        setUserStyle({});
-    }
+  const handlePageChange = (index: number) => {
+    setCurrentQuestionIndex(index);
+    setBaseStyle(questions[index].initialCSS);
+    setUserStyle({});
+  };
 
-    const combinedStyle = { ...baseStyle, ...userStyle };
-    const fetchHtml = () => {
-        return "";
-    }
+  const combinedStyle = { ...baseStyle, ...userStyle };
+  const fetchHtml = () => {
+    return "";
+  };
 
-    return (
-        <div className="flex min-h-[100vh] select-none flex-col-reverse items-center justify-around py-10 lg:flex-row xl:py-0 ">
-            {/* Question editor */}
-            <div className="order-2 md:order-1 flex-1 justify-center mx-auto h-full w-[98%] max-w-[1000px] px-12 py-20 lg:ml-24 lg:mr-0">
+  return (
+    <div className="flex min-h-[100vh] select-none flex-col-reverse items-center justify-around py-10 lg:flex-row xl:py-0">
+      {/* Question editor */}
+      <div className="order-2 md:order-1 flex-1 justify-center mx-auto h-full w-[98%] max-w-[1000px] px-12 py-20 lg:ml-24 lg:mr-0">
+        <div className="mx-auto flex min-w-full flex-col items-center justify-between gap-8 md:flex-row">
+          <QuestionDisplay
+            question={questions[currentQuestionIndex]}
+            currentIndex={currentQuestionIndex}
+            totalQuestions={questions.length}
+            onPageChange={handlePageChange}
+          />
+        </div>
 
-                <div className="mx-auto flex max-w-full flex-col items-center justify-between md:flex-row">
-                    <QuestionDisplay
-                        question={questions[currentQuestionIndex]}
-                        currentIndex={currentQuestionIndex}
-                        totalQuestions={questions.length}
-                        onPageChange={handlePageChange}
-                    />
-                </div>
+        <p className="flex justify-center mx-auto my-12 max-w-xl">
+          {questions[currentQuestionIndex].instruction}
+        </p>
 
-                <p className="flex justify-center mx-auto my-12 max-w-xl ">{questions[currentQuestionIndex].instruction}</p>
+        <CSSEditor baseStyle={baseStyle} onStyleChange={handleStyleChange} />
+      </div>
 
-                <CSSEditor
-                    baseStyle={baseStyle}
-                    onStyleChange={handleStyleChange}
-                />
-            </div>
-
-            {/* Playground */}
-            <div className="lg:mx-36 order-2 md:order-1 relative h-[300px] w-[300px] rounded-xl bg-cover lg:h-[500px] lg:w-[500px] xl:h-[550px] xl:w-[550px] border-black border bg-center bg-no-repeat">
-                <PreviewBox html={fetchHtml} style={combinedStyle} />
-            </div>
-        </div >
-    );
-}
+      {/* Playground */}
+      <div className="lg:mx-36 order-2 md:order-1 relative h-[300px] w-[300px] rounded-xl bg-cover lg:h-[500px] lg:w-[500px] xl:h-[550px] xl:w-[550px] border-black border bg-center bg-no-repeat">
+        <PreviewBox html={fetchHtml} style={combinedStyle} />
+      </div>
+    </div>
+  );
+};
 
 export default Main;
-
--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,91 +1,93 @@
-'use client'
+"use client";
 import Link from "next/link";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import CountdownTimer from "./CountdownTimer";
-import QuestionPalette from "@/components/QuestionPallete"
-import questionsData from "@/public/objectStyle.json"
+import QuestionPalette from "@/components/QuestionPallete";
+import questionsData from "@/public/objectStyle.json";
 
 const Topbar = () => {
-    const [showPalette, setShowPalette] = useState(false);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [attemptedQuestions, setAttemptedQuestions] = useState<Set<number>>(new Set());
-    const handleTimeUp = () => {
-        alert("Time's up");
-    }
+  const [showPalette, setShowPalette] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [attemptedQuestions, setAttemptedQuestions] = useState<Set<number>>(
+    new Set()
+  );
+  const handleTimeUp = () => {
+    alert("Time's up");
+  };
 
-    const togglePalette = () => {
-        setShowPalette(!showPalette);
-    };
-    const handleQuestionSelect = (index: number) => {
-        setCurrentQuestionIndex(index);
-        setAttemptedQuestions(prev => new Set(prev.add(index)));
-    };
+  const togglePalette = () => {
+    setShowPalette(!showPalette);
+  };
+  const handleQuestionSelect = (index: number) => {
+    setCurrentQuestionIndex(index);
+    setAttemptedQuestions((prev) => new Set(prev.add(index)));
+  };
 
-    return (
-        <div className="bg-primary min-w-full bg-slate-50 py-2 px-5 flex items-center justify-between border-b border-black">
-            {/* Just hide the name flexing css if the display is small. Can't seem to fit all stuff if display is small*/}
-            <div className="hidden md:block items-center flex">
-                <Link href="/" className="font-bold text-xl flex items-center">
-                    Flexing CSS
-                </Link>
-            </div>
+  return (
+    <div className="bg-primary min-w-full bg-slate-50 py-2 px-5 flex items-center justify-between border-b border-black">
+      {/* Just hide the name flexing css if the display is small. Can't seem to fit all stuff if display is small*/}
+      <div className="hidden md:block items-center sm:flex">
+        <Link href="/" className="font-bold text-xl flex items-center">
+          Flexing CSS
+        </Link>
+      </div>
 
-            <div className="flex justify-center">
-                <div className="flex flex-row items-center">
-                    <div>
-                        <button
-                            onClick={togglePalette}
-                            className="mr-4 bg-black text-white px-3 py-1 rounded text-sm"
-                        >
-                            Show Questions
-                        </button>
+      <div className="flex justify-center">
+        <div className="flex flex-row items-center">
+          <div>
+            <button
+              onClick={togglePalette}
+              className="mr-4 bg-black text-white px-3 py-1 rounded text-sm"
+            >
+              Show Questions
+            </button>
 
-                        {showPalette && (
-                            <QuestionPalette
-                                questions={questionsData}
-                                currentQuestionIndex={currentQuestionIndex}
-                                attemptedQuestions={attemptedQuestions}
-                                onQuestionSelect={handleQuestionSelect}
-                                onClose={togglePalette}
-                            />
-                        )}
-                    </div>
-                    <div className="mx-2">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger className="focus:outline-none">
-                                <Avatar>
-                                    <AvatarImage src="https://github.com/shadcn.png" />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent className="bg-slate-50 shadow-lg">
-                                <DropdownMenuLabel>Username</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem>
-                                    <Link className="text-red-500 font-bold" href="/end-test">
-                                        End-Test
-                                    </Link>
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </div>
+            {showPalette && (
+              <QuestionPalette
+                questions={questionsData}
+                currentQuestionIndex={currentQuestionIndex}
+                attemptedQuestions={attemptedQuestions}
+                onQuestionSelect={handleQuestionSelect}
+                onClose={togglePalette}
+              />
+            )}
+          </div>
+          <div className="mx-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="focus:outline-none">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-slate-50 shadow-lg">
+                <DropdownMenuLabel>Username</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link className="text-red-500 font-bold" href="/end-test">
+                    End-Test
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
 
-                    <div className="ml-4 px-2 py-1 border-black border-2 rounded ">
-                        <CountdownTimer initialTime={300} onTimeUp={handleTimeUp} />
-                    </div>
-                </div>
-            </div>
+          <div className="ml-4 px-2 py-1 border-black border-2 rounded ">
+            <CountdownTimer initialTime={300} onTimeUp={handleTimeUp} />
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Topbar;
