c5f4007decc5dae5a7860788b2483c0d54be1258
QuantiniumX <vaibhavvx331@gmail.com>
2024-08-04 17:35:33 +0530

    added loading svg and styled it

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Image from "next/image"
+import loadingSVG from "@/public/loading.svg"
 
 export default function loading() {
-  return <div>loading</div>;
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <Image src={loadingSVG} width={30} height={30} alt="loading" />
+      <span className="ml-4 text-2xl">Loading</span>
+    </div>
+  );
 }
