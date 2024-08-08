bdcf07dfe6a50da76db9d315b2ad448df5014ee5
Vanshika Aggarwal <vanshikaaggarwal1501@gmail.com>
2024-08-08 20:24:54 +0530

    added questions

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -32,14 +32,16 @@ function Planet({
 }
 
 function Ring({
+  type,
   variant,
   className,
 }: {
+  type?: "absolute" | "normal";
   variant: "blue" | "red" | "green" | "white";
   className?: String;
 }) {
   const ringVariant = cva(
-    "absolute border-[5px] border-solid rounded-full w-[50px] h-[50px] lg:w-[70px] lg:h-[70px] transform translate-x-[-50%] translate-y-[-50%] drop-shadow-xl opacity-90 animate-pulse z-10 rotate-x-45 translate-z-50",
+    "border-[5px] border-solid rounded-full w-[50px] h-[50px] lg:w-[70px] lg:h-[70px] transform translate-x-[-50%] translate-y-[-50%] drop-shadow-xl opacity-90 animate-pulse z-10 rotate-x-45 translate-z-50",
     {
       variants: {
         variant: {
@@ -48,21 +50,28 @@ function Ring({
           red: "border-[rgba(128,0,0,0.7)]",
           white: "border-[rgba(255,255,255,0.7)]",
         },
+        type: {
+          absolute: "absolute",
+          normal: "static",
+        },
       },
       defaultVariants: {
         variant: "blue",
+        type: "absolute",
       },
     },
   );
 
   return (
     <div
-      className={cn(ringVariant({ variant }), className)}
+      className={cn(ringVariant({ variant, type }), className)}
       style={{
         transformStyle: "preserve-3d", // Adjust or remove if unnecessary
       }}
-    ></div>
-  );
+    >
+
+  </div>
+  );
 }
 
 interface QuestionsType {
@@ -131,29 +140,6 @@ export const Questions: QuestionsType = {
     );
   },
 
-  easy_4: ({ inputStyle }: { inputStyle: string }) => {
-    return (
-      <>
-        <div
-          id="container"
-          className="flex h-full p-16"
-          style={inputStyle ? JSON.parse(inputStyle) : {}}
-        >
-           <Planet variant="red" />
-          <Planet variant="green" />
-          <Planet variant="blue" />
-          <Planet variant="white" />
-        </div>
-        <div id="master" className="absolute inset-0 flex p-16 ">
-          <Ring variant="red" className="bottom-[28%] right-[4.5%]" />
-          <Ring variant="green" className="bottom-[17%] right-[4.5%]" />
-          <Ring variant="blue" className="bottom-[7%] right-[4.5%]" />
-          <Ring variant="white" className="bottom-[2%] right-[4.5%]" />
-        </div>
-      </>
-    );
-  },
-
   easy_8: ({ inputStyle }: { inputStyle: string }) => {
     return (
       <>
@@ -162,15 +148,15 @@ export const Questions: QuestionsType = {
           className="absolute inset-0 flex p-16"
           style={inputStyle ? JSON.parse(inputStyle) : {}}
         >
-          <Planet variant="blue" />
+         <Planet variant="blue" />
           <Planet variant="green" />
           <Planet variant="red" />
         </div>
-        <div id="master" className="absolute inset-0 p-16">
-          <Ring variant="red" className="top-[74%] left-[11%]" />
-          <Ring variant="green" className="top-[74%] left-[43.8%]" />
-          <Ring variant="blue" className="top-[74%] left-[76.4%]" />
-        </div>
+        <div id="master" className="absolute inset-0 flex p-16">
+          <Ring variant="red" className="top-[78.5%] left-[19%]" />
+          <Ring variant="green" className="top-[78.5%] left-[50%]" />
+          <Ring variant="blue" className="top-[78.5%] left-[81%]" />
+      </div>
       </>
     );
   },
@@ -186,10 +172,10 @@ export const Questions: QuestionsType = {
           <Planet variant="green" />
           <Planet variant="red" />
         </div>
-        <div id="master" className="absolute inset-0 p-16">
-          <Ring variant="blue" className="top-[51.5%] left-[11%]" />
-          <Ring variant="green" className="top-[63.5%] left-[11%]" />
-          <Ring variant="red" className="top-[75.5%] left-[11%]" />
+        <div id="master" className="absolute inset-0 flex p-16">
+          <Ring variant="blue" className="top-[50%] left-[12%]" />
+          <Ring variant="green" className="top-[60.5%] left-[12%]" />
+          <Ring variant="red" className="top-[72%] left-[12%]" />
         </div>
       </>
     );
@@ -202,10 +188,12 @@ export const Questions: QuestionsType = {
           className="absolute inset-0 flex p-16"
           style={inputStyle ? JSON.parse(inputStyle) : {}}
         >
-          <Planet variant="blue" />
+       <Planet variant="blue" />
+ 
         </div>
-        <div id="master" className="absolute inset-0 p-16">
-          <Ring variant="blue" className="top-[41.5%] left-[43.5%]" />
+        <div id="master" className="absolute inset-0 flex p-16">
+          <Ring variant="blue" className="top-[42%] left-[43%]" />
+    
         </div>
       </>
     );
@@ -218,14 +206,14 @@ export const Questions: QuestionsType = {
           className="absolute inset-0 flex p-16"
           style={inputStyle ? JSON.parse(inputStyle) : {}}
         >
-          <Planet variant="white" />
+         <Planet variant="white" />
           <Planet variant="green" />
           <Planet variant="red" />
         </div>
-        <div id="master" className="absolute inset-0 p-16">
-          <Ring variant="red" className="top-[8.3%] left-[11%]" />
-          <Ring variant="green" className="top-[8.3%] left-[24%]" />
-          <Ring variant="white" className="top-[8.3%] left-[37%]" />
+        <div id="master" className="absolute inset-0 flex p-16">
+          <Ring variant="red" className="top-[11%] left-[13%]" />
+          <Ring variant="green" className="top-[11%] left-[24.5%]" />
+          <Ring variant="white" className="top-[11%] left-[36.5%]" />
         </div>
       </>
     );
@@ -387,7 +375,54 @@ export const Questions: QuestionsType = {
       </>
     );
   },
+  easy_20: ({ inputStyle }: { inputStyle: string }) => {
+    return (
+      <>
+        <div
+          id="container"
+          className="absolute inset-0 flex p-16"
+          style={inputStyle ? JSON.parse(inputStyle) : {}}
+        >
+          <Planet variant="blue" />
+          <Planet variant="green" />
+          <Planet variant="red" />
+        </div>
+        <div id="master" className="absolute inset-0 flex p-16">
+          <Ring variant="green" className="top-[72%] right-[61%]" /> 
+          <Ring variant="red" className="top-[72%] right-[49%]" />
+          <Ring variant="blue" className="top-[72%] right-[73.5%]" />
+
+        </div>
+      </>
+    );
+  },
+
+  easy_21: ({ inputStyle }: { inputStyle: string }) => {
+    return (
+      <>
+        <div
+          id="container"
+          className="absolute inset-0 flex p-16"
+          style={inputStyle ? JSON.parse(inputStyle) : {}}
+        >
+           <Planet variant="blue" />
+          <Planet variant="green" />
+          <Planet variant="red" />
+        </div>
+        <div id="master" className="absolute inset-0 flex p-16">
+          <Ring variant="green" className="top-[11%] right-[23.5%]" /> 
+          <Ring variant="red" className="top-[11%] right-[37%]" />
+          <Ring variant="blue" className="top-[11%] right-[10.8%]" />
+
+        </div>
+      </>
+    );
+  },
 
 };
+
+
+
+
 
 
