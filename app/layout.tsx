e1d1257d5daf8dc801a153d1c7b77fbd0d7f479e
quantinium <72118517+QuantiniumX@users.noreply.github.com>
2024-08-20 01:35:51 +0530

    changed name from Flexmania to Practice-Flexmania

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FlexMania",
+  title: "Practice-FlexMania",
   description: "Website made for a CSS contest in college",
 };
 
